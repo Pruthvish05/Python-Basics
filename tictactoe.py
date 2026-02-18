7727814d9ee7dd54608ead721d1e59791ada190a
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-02-19 01:08:58 +0530

    i still am not thorrough with this this file is not IT! i will come bac to this

--- tictactoe.py
+++ tictactoe.py
@@ -3,7 +3,7 @@ def box():
         print("****"*4)
         for j in range(3):
             print("|    "*4)
-box()
+#box()
 def tic_tac_toe():
     i=int(input("Enter row number (0-2): "))
     j=int(input("Enter column number (0-2): "))
@@ -18,5 +18,28 @@ def tic_tac_toe():
                         else:
                             print("- ", end="")
                         print()
-tic_tac_toe()
+
+def tic_tac_toe1():
+    board=[['-','-','-'],['-','-','-'],['-','-','-']]
+    lst=[]
+    i1=int(input("Enter row number of player 1 (0-2): "))
+    j1=int(input("Enter column number of player 1 (0-2): "))
+    i2=int(input("Enter row number of player 2 (0-2): "))
+    j2=int(input("Enter column number of player 2 (0-2): "))
+    if i1<0 or i1>2 or j1<0 or j1>2:
+        print("Invalid input for player 1. Please enter numbers between 0 and 2.")
+    elif i2<0 or i2>2 or j2<0 or j2>2:
+        print("Invalid input for player 2. Please enter numbers between 0 and 2.")
+    elif i1==i2 and j1==j2:
+        print("Both players cannot choose the same position. Please try again.")
+    else:
+        board[i1][j1]='X'
+        board[i2][j2]='O'
+    for i in range(3):
+        for j in range(3):
+            print(board[i][j],end=" ")
+        print()
+    lst.append((i1,j1))
+    lst.append((i2,j2))
+tic_tac_toe1()
 
