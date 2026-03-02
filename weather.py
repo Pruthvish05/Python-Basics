0ec0754d197df2f0f42a90190b2f0db75cf6ec4f
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-03-02 22:40:47 +0530

    created weather.py gives out the weather in the city of your choice.

---
+++ weather.py
@@ -0,0 +1,20 @@
+import requests
+from pprint import pprint
+def get_weather(city):
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&appid=ee0157f47aa120cede525459862788d9&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    if response.status_code == 200:
+        weather_info = {
+            "City": data["name"],
+            "Temperature": data["main"]["temp"],
+            "Weather": data["weather"][0]["description"],
+            "Humidity": data["main"]["humidity"],
+            "Wind Speed": data["wind"]["speed"]
+        }
+        return weather_info
+    else:
+        return {"Error": data.get("message", "An error occurred")}
+city = input("Enter the city name: ")
+weather = get_weather(city)
+pprint(weather)
