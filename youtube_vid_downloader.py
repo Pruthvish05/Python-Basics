84dfd3677ba59fd10ed0c0ccaab76df51c142354
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-03-06 23:05:48 +0530

    Update youtube_vid_downloader.py
    in this i used yt_dlp library for downloading a video from youtube created a function which takes in url and name
    ydl_opts creates a dictionary value of our settings in 'format' is set to bestvideo and bestaudio this results in both video and audio
    outml helps in storing the file in downloads with an automatic extension adder
    merging the whole output with mp4 tag creating an object of this whole new settings and using them with the help of .download[]
    taking user inputs and calling the functions

--- youtube_vid_downloader.py
+++ youtube_vid_downloader.py
@@ -1,1 +1,14 @@
-import 
+import yt_dlp
+def download_video(url,name):
+    ydl_opts = {
+        'format': 'bestvideo+bestaudio/best',
+        'outtmpl': 'downloads/' + name + '.%(ext)s',
+        'merge_output_format': 'mp4',
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+url = input("Enter the YouTube video URL: ")
+name = input("Enter the desired name for the downloaded video (without extension): ")
+download_video(url,name)
+
+
