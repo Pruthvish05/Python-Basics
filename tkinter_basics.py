541a287e42afa5e9b12c9ce7f6d64c3777f55406
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-03-10 01:19:53 +0530

    Update tkinter_basics.py

--- tkinter_basics.py
+++ tkinter_basics.py
@@ -1,4 +1,9 @@
 from tkinter import *
 first_var = Tk()
 first_var.title("First GUI")
+first_var.geometry("400x400")
+first_var.config(bg="lightblue")
+first_var.maxsize(500, 500)
+first_var.minsize(200, 200)
+
 start = mainloop()
