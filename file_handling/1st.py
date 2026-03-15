4069c37f9be27f4fcbcfd1578e9b207007b0deef
Pruthvish05 <pruthvishkulkarni572@gmail.com>
2026-03-15 11:18:10 +0530

    created a file_handling file

---
+++ file_handling/1st.py
@@ -0,0 +1,8 @@
+#file
+# op = open("file_handling/1st.txt", "w")
+# op.write("This is my first file handling program in python")
+# op.close()
+#read
+op = open("file_handling/1st.txt", "r")
+print(op.read())
+op.close()
